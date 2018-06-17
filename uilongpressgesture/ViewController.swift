d34aa0c01bd66353347eab5e74e7a327f9cacbb4
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-06-17 13:07:41 +0300

    new implementation.

--- uilongpressgesture/ViewController.swift
+++ uilongpressgesture/ViewController.swift
@@ -11,31 +11,93 @@ import UIKit
 class ViewController: UIViewController
 {
     @IBOutlet var viewToPress: UIView!
+    @IBOutlet var progressBar: UIProgressView!
     
     var longGesture = UILongPressGestureRecognizer()
+    
+    var intCounter = Double()
+    var timer = Timer()
+    
+    
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
         longGesture = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.triggeredAction(_:)))
-        longGesture.minimumPressDuration = 6
-        longGesture.allowableMovement = 99999999
+        
+        //longGesture.allowableMovement = 99999999
         
         viewToPress.addGestureRecognizer(longGesture)
         
+    }
+    
+ 
+    
+    
+    @objc func triggeredAction(_ sender: UILongPressGestureRecognizer)
+    {
+        
+        if sender.state == UIGestureRecognizerState.began
+        {
+            print("start")
+            viewToPress.backgroundColor = UIColor.green
+            
+            runTimer()
+        }
+        else if sender.state == UIGestureRecognizerState.ended
+        {
+            print("ended")
+            viewToPress.backgroundColor = UIColor.red
+            
+            timer.invalidate()
+            intCounter=0;
+        }
         
         
     }
     
-    @objc func triggeredAction(_ sender: UILongPressGestureRecognizer)
+    
+    
+    
+    
+    
+    func initializeTimer()
     {
-        let alertC = UIAlertController(title: "LongPressGestureRecognizer", message: "LongPressGesture was triggered", preferredStyle: UIAlertControllerStyle.alert)
-        let ok = UIAlertAction(title: "okay", style: UIAlertActionStyle.default) { (alert) in
-        }
-        alertC.addAction(ok)
-        self.present(alertC, animated: true, completion: nil)
+      runTimer()
+        
     }
+    
+    func runTimer()
+    {
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateCountdown), userInfo: nil, repeats: true)
+    }
+    
+    
+    func reset()
+    {
+       runTimer()
+    }
+    
+    @objc func updateCountdown()
+    {
+        intCounter += 0.1
+        
+        
+        
+        //Set counter in UILabel
+        print(intCounter)
+        
+        if intCounter <= 0
+        {
+            timer.invalidate()
+
+        }
+    }
+
+    
+    
     
     
 
